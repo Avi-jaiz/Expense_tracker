8c99f22ae2a2d77929c799001ab5864498369e5e
Avishekh Jaiswal <j.avishekh2021@gmail.com>
2022-08-24 17:19:27 +0530

    'rendering list completed'

--- src/App.js
+++ src/App.js
@@ -5,19 +5,18 @@ import NewExpenses from './components/NewExpenses/NewExpenses';
 import ExpensesFilter from './components/ExpensesFilter';
 import { useState } from 'react';
 
-function App() {
- 
-const [filteredYear,setFilteredYear] =useState('2020')
 
-  const expenseData = [
-    {id:1,title:'Insurance', amount:1200,date: new Date(2022,8,21)},
-    
-    {id:2,title:'Home Loan', amount:15000,date: new Date(2022,8,15)},
-    {id:3,title:'Groceries', amount:7500,date: new Date(2022,8,2)},
-    {id:4,title:'Medicines', amount:9000,date: new Date(2022,8,5)},
-
+const initialData = [
+  {id:1,title:'Udhar', amount:150,date: new Date(2022,8,21)},
 
 ]
+
+function App() {
+ 
+const [filteredYear,setFilteredYear] =useState('2020');
+const [expenses,setExpenses]  = useState(initialData)
+
+
 
 const addExpenseHandler =(expenses)=>
 {
@@ -25,8 +24,10 @@ const addExpenseHandler =(expenses)=>
         ...expenses,
         
       }
-      console.log(addedExpense)
-      console.log('from App.js')
+     
+      setExpenses((prevState)=>{
+        return [addedExpense,...prevState]
+      })
 }
 
 const filterChangeHandler = (selectedYear)=>
@@ -40,39 +41,25 @@ console.log(filteredYear)
       <NewExpenses onAddExpense ={addExpenseHandler}/>
       <ExpensesFilter  selected={filteredYear} onChangeFilter = {filterChangeHandler}/>
 
+
+
+{expenses.map((item,index)=>
+{
+ 
+   return(
+    <div key ={expenses[index].id.toString()}>
 <ExpenseItems 
-title = {expenseData[0].title}
-amount = {expenseData[0].amount}
+title = {item.title}
+amount = {item.amount}
 
-date ={expenseData[0].date}
-
-
+date ={item.date} 
 
 />
- <ExpenseItems
- title = {expenseData[1].title}
- amount = {expenseData[1].amount}
- year = {expenseData[1].date}
- month = {expenseData[1].date}
- date ={expenseData[1].date}
- />   
- <ExpenseItems 
- title = {expenseData[2].title}
- amount = {expenseData[2].amount}
- year = {expenseData[2].date}
- month = {expenseData[2].date}
- date ={expenseData[2].date}
- />
- <ExpenseItems 
- title = {expenseData[3].title}
- amount = {expenseData[3].amount}
- year = {expenseData[3].date}
- month = {expenseData[3].date}
- date ={expenseData[3].date}
- />    
+    </div>
 
-
-
+   
+   )
+})}
     
     
     </Card>
